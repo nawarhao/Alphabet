dcbd0de720e87132f819ba897e8e718231c28812
razonno <nrazonno@gmail.com>
2025-05-03 12:23:34 -0400

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+char = input("Enter a character: ")
+
+if 'a' <= char <= 'z' or 'A' <= char <= 'Z':
+    print("The character is an alphabet.")
+else:
+    print("The character is not an alphabet.")
